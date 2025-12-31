1d20f434eafba2987fcbe9ae25bfcce1d15a4fe5
hanyganal72 <hany.gamal290@gmail.com>
2025-12-31 15:53:15 +0200

    feat: add global styles and custom font support for Hijri-Gregorian Datepicker

--- projects/hijri-gregorian-datepicker/src/public-api.ts
+++ projects/hijri-gregorian-datepicker/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/hijri-gregorian-datepicker.component';
 export * from './lib/hijri-gregorian-datetime-input.component'; // NEW: Export datetime input component
 export * from './lib/hijri-gregorian-datepicker.module';
 export * from './_interfaces/calendar-model'; // Export interfaces including TimeValue
+export * from './_interfaces/styles-config.model'; // Export stylesConfig interface
